a4928a703154bb33c10a690cfaf380edddb8280f
siph <dawkins.chris.dev@gmail.com>
2021-11-28 07:56:10 -0700

    feature: add getAlbums endpoint

--- src/main/kotlin/com/vidbox/controller/AlbumController.kt
+++ src/main/kotlin/com/vidbox/controller/AlbumController.kt
@@ -3,10 +3,12 @@ package com.vidbox.controller
 import com.vidbox.model.Album
 import com.vidbox.repository.AlbumRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.security.Principal
 
@@ -17,5 +19,15 @@ class AlbumController(@Autowired private val albumRepository: AlbumRepository) {
     fun createAlbum(principal: Principal): ResponseEntity<Any> {
         val album = Album(owner = principal.name)
         return ResponseEntity(albumRepository.save(album), HttpStatus.OK)
+    }
+
+    @RequestMapping(value = ["/albums"], method = [RequestMethod.GET])
+    fun getAlbums(@RequestParam(value = "page", required = false, defaultValue = "1")
+                  page: Int,
+                  @RequestParam(value = "size", required = false, defaultValue = "20")
+                  pageSize: Int,
+                  principal: Principal): ResponseEntity<Any> {
+        val albums = albumRepository.findAllByOwner(principal.name, PageRequest.of(page, pageSize))
+        return ResponseEntity(albums, HttpStatus.OK)
     }
 }
--- src/main/kotlin/com/vidbox/repository/AlbumRepository.kt
+++ src/main/kotlin/com/vidbox/repository/AlbumRepository.kt
@@ -1,7 +1,10 @@
 package com.vidbox.repository
 
 import com.vidbox.model.Album
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AlbumRepository: JpaRepository<Album, Long> {
+    fun findAllByOwner(owner: String, pageable: Pageable): Page<Album>
 }
