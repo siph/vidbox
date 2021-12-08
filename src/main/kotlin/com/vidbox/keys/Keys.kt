f3c9ad7a53a24cf2b87d2b8fc1ae1bee7a5b0902
siph <dawkins.chris.dev@gmail.com>
2021-12-07 20:28:27 -0700

    bugfix: add entity annotation for component managment

--- src/main/kotlin/com/vidbox/keys/Keys.kt
+++ src/main/kotlin/com/vidbox/keys/Keys.kt
@@ -1,7 +1,11 @@
 package com.vidbox.keys
 
+import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 
+@Entity
+@Table(name = "keys")
 class Keys(@Id val owner: String,
            var telegramApiKey: String? = null)
 
