7bd879da8cee1d802bb23c17d79297bc76d146c9
siph <dawkins.chris.dev@gmail.com>
2021-12-13 16:14:05 -0700

    logging: album service logging

--- src/main/kotlin/com/vidbox/album/AlbumService.kt
+++ src/main/kotlin/com/vidbox/album/AlbumService.kt
@@ -1,19 +1,24 @@
 package com.vidbox.album
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
-import java.security.Principal
 
 @Service
 class AlbumService(@Autowired private val albumRepository: AlbumRepository) {
+    companion object {
+        val log = LoggerFactory.getLogger(AlbumService::class.java)
+    }
 
     fun getAlbums(owner: String, pageRequest: PageRequest): Page<Album> {
+        log.debug("get albums request for owner: $owner")
         return albumRepository.findAllByOwner(owner, pageRequest)
     }
 
     fun createAlbum(owner: String): Album {
+        log.debug("create album request for owner $owner")
         val album = Album(owner = owner)
         return albumRepository.save(album)
     }
