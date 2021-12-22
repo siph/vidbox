abeb428ef7d4b580ced766ef2b35e4fbbb221015
siph <dawkins.chris.dev@gmail.com>
2021-12-22 07:46:48 -0700

    logging: resttemplate bean creation

--- src/main/kotlin/com/vidbox/VidboxApplication.kt
+++ src/main/kotlin/com/vidbox/VidboxApplication.kt
@@ -4,13 +4,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
+import org.slf4j.LoggerFactory
 
 @SpringBootApplication
 class VidboxApplication{
-
+    companion object {
+            val log = LoggerFactory.getLogger(VidboxApplication::class.java)
+        }
     @Bean
     fun getRestTemplate(): RestTemplate {
-        return RestTemplate()
+        val restTemplate = RestTemplate()
+        log.debug("creating restTemplate bean")       
+        log.debug("$restTemplate")
+        return restTemplate
     }
 }
 
