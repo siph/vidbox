fc2719142be372c402f6b6d082e50e0830fce4d9
siph <dawkins.chris.dev@gmail.com>
2022-01-01 11:54:25 -0700

    test: album service coverage

--- src/test/kotlin/com/vidbox/album/AlbumServiceTests.kt
+++ src/test/kotlin/com/vidbox/album/AlbumServiceTests.kt
@@ -1,6 +1,7 @@
 package com.vidbox.album
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,9 +10,24 @@ import org.springframework.data.domain.PageRequest
 @SpringBootTest
 class AlbumServiceTests(@Autowired private val albumService: AlbumService) {
 
+    val owner = "test"
+
     @Test
-    fun `assert that albums are created`() {
-        albumService.createAlbum("test")
-        assertThat(albumService.getAlbums("test", PageRequest.of(1, 10)).totalElements).isEqualTo(1)
+    @Order(1)
+    fun `assert that album is created and retrieved`() {
+        val album = albumService.createAlbum(owner)
+        assertThat(albumService.getAlbum(album.owner, album.id))
+            .usingRecursiveComparison().isEqualTo(album)
+    }
+
+    @Test
+    @Order(2)
+    fun `assert that list is retrieved and album is deleted`() {
+        albumService.createAlbum(owner)
+        val albums = albumService.getAlbums(owner, PageRequest.of(0, 10))
+        assertThat(albums.totalElements).isEqualTo(1)
+        val album = albums.get().findFirst().orElseThrow { Exception() }
+        albumService.deleteAlbum(album.owner, album.id)
+        assertThat(albumService.getAlbums(owner, PageRequest.of(1, 10)).totalElements).isEqualTo(0)
     }
 }
