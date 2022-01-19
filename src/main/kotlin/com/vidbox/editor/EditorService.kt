684bbc88679a7bffbd25bc4e3b0d1cd35b77f6d7
siph <dawkins.chris.dev@gmail.com>
2022-01-19 15:05:56 -0700

    feature: concat videos

--- src/main/kotlin/com/vidbox/editor/EditorService.kt
+++ src/main/kotlin/com/vidbox/editor/EditorService.kt
@@ -1,15 +1,30 @@
 package com.vidbox.editor
 
+import com.vidbox.album.Album
+import com.vidbox.album.AlbumService
+import com.vidbox.file.File
+import com.vidbox.file.FileService
+import com.vidbox.util.deleteTempFile
+import com.vidbox.util.getTempFile
+import org.apache.commons.fileupload.disk.DiskFileItem
+import org.apache.commons.io.IOUtils
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.multipart.commons.CommonsMultipartFile
 import java.io.BufferedReader
+import java.io.FileInputStream
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.nio.file.Files
 import java.util.function.Consumer
+import java.util.stream.Collectors
 import javax.annotation.PostConstruct
 
 @Service
-class EditorService {
+class EditorService(@Autowired private val albumService: AlbumService,
+                    @Autowired private val fileService: FileService) {
     companion object {
         val log = LoggerFactory.getLogger(EditorService::class.java)
     }
@@ -18,6 +33,20 @@ class EditorService {
     fun init() {
         validateClippit()
         validateCattit()
+    }
+
+    //TODO: make and delete directory
+    private fun cattitAlbum(album: Album, newFileName: String): File {
+        val rootPath = "/tmp/${album.owner}"
+        val files: List<java.io.File> = album.files.stream()
+            .map { getTempFile(it, fileService.getContentByFile(it), rootPath) }
+            .collect(Collectors.toList())
+        getProcessBuilder(listOf("cattit", "-i", rootPath, "-t", newFileName)).start()
+        val newFile = fileService.uploadFile(
+            getMultipartFile(java.io.File("$rootPath/$newFileName")),
+            album.owner)
+        files.forEach { deleteTempFile(it) }
+        return newFile
     }
 
     private fun validateCattit(): Boolean {
@@ -54,6 +83,20 @@ class EditorService {
         val processBuilder = ProcessBuilder()
         processBuilder.command(command)
         return processBuilder
+    }
+
+    private fun getMultipartFile(file: java.io.File): MultipartFile {
+        val fileItem = DiskFileItem(
+            "file",
+            Files.probeContentType(file.toPath()),
+            false,
+            file.name,
+            file.length().toInt(),
+            file.parentFile)
+        val inputStream = FileInputStream(file)
+        val outputStream = fileItem.outputStream
+        IOUtils.copy(inputStream, outputStream)
+        return CommonsMultipartFile(fileItem)
     }
 
 }
