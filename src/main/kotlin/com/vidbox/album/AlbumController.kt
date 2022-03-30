58be03e93c031dbf3bd26935088ce6770d8c9fe1
siph <dawkins.chris.dev@gmail.com>
2022-03-29 18:42:15 -0600

    doc: swagger documentation for album controller

--- src/main/kotlin/com/vidbox/album/AlbumController.kt
+++ src/main/kotlin/com/vidbox/album/AlbumController.kt
@@ -1,5 +1,11 @@
 package com.vidbox.album
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
@@ -14,14 +20,34 @@ import java.security.Principal
 @RestController
 class AlbumController(@Autowired private val albumService: AlbumService) {
 
+    @Operation(summary = "Create empty album")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "201",
+            description = "Album created",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Album::class))]
+        )
+    )
     @RequestMapping(value = ["/album"], method = [RequestMethod.POST])
-    fun createAlbum(principal: Principal): ResponseEntity<Any> {
+    fun createAlbum(principal: Principal): ResponseEntity<Album> {
         return ResponseEntity(albumService.createAlbum(principal.name), HttpStatus.CREATED)
     }
 
+    @Operation(summary = "Get users albums")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Albums retrieved",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Album::class))]
+        )
+    )
     @RequestMapping(value = ["/albums"], method = [RequestMethod.GET])
-    fun getAlbums(@RequestParam(value = "page", required = false, defaultValue = "1")
+    fun getAlbums(@Parameter(description = "Page number")
+                  @RequestParam(value = "page", required = false, defaultValue = "1")
                   page: Int,
+                  @Parameter(description = "Results per page")
                   @RequestParam(value = "size", required = false, defaultValue = "20")
                   pageSize: Int,
                   principal: Principal): ResponseEntity<Page<Album>> {
@@ -29,36 +55,78 @@ class AlbumController(@Autowired private val albumService: AlbumService) {
         return ResponseEntity(albums, HttpStatus.OK)
     }
 
+    @Operation(summary = "Add file to album")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Successfully added",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Album::class))]
+        )
+    )
     @RequestMapping(value = ["/album/add"], method = [RequestMethod.POST, RequestMethod.PUT])
     fun addFileToAlbum(principal: Principal,
+                       @Parameter(description = "Id for requested file")
                        @RequestParam(value = "fileId")
                        fileId: Long,
+                       @Parameter(description = "Id for requested album")
                        @RequestParam(value = "albumId")
                        albumId: Long): ResponseEntity<Album> {
         val album = albumService.addFileToAlbum(owner = principal.name, albumId = albumId, fileId = fileId)
         return ResponseEntity(album, HttpStatus.OK)
     }
 
+    @Operation(summary = "Remove file from album")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Successfully removed",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Album::class))]
+        )
+    )
     @RequestMapping(value = ["/album/remove"], method = [RequestMethod.POST, RequestMethod.DELETE])
     fun deleteFileFromAlbum(principal: Principal,
-                       @RequestParam(value = "fileId")
-                       fileId: Long,
-                       @RequestParam(value = "albumId")
-                       albumId: Long): ResponseEntity<Album> {
+                            @Parameter(description = "Id for requested file")
+                            @RequestParam(value = "fileId")
+                            fileId: Long,
+                            @Parameter(description = "Id for requested album")
+                            @RequestParam(value = "albumId")
+                            albumId: Long): ResponseEntity<Album> {
         val album = albumService.deleteFileFromAlbum(owner = principal.name, albumId = albumId, fileId = fileId)
         return ResponseEntity(album, HttpStatus.OK)
     }
 
+    @Operation(summary = "Get album")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Successfully retrieved",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Album::class))]
+        )
+    )
     @RequestMapping(value = ["/album"], method = [RequestMethod.GET])
     fun getAlbum(principal: Principal,
+                 @Parameter(description = "Id for requested album")
                  @RequestParam(value = "id")
                  id: Long): ResponseEntity<Album> {
         val album = albumService.getAlbum(owner = principal.name, albumId = id)
         return ResponseEntity(album, HttpStatus.OK)
     }
 
+    @Operation(summary = "Delete album")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Successfully deleted",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Album::class))]
+        )
+    )
     @RequestMapping(value = ["/album"], method = [RequestMethod.DELETE])
     fun deleteAlbum(principal: Principal,
+                    @Parameter(description = "Id for requested album")
                     @RequestParam(value = "id")
                     id: Long): ResponseEntity<Any> {
         albumService.deleteAlbum(owner = principal.name, albumId = id)
