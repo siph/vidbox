d41daeced7c00acc8f9c6b9f85a6db8f61c8eac4
siph <dawkins.chris.dev@gmail.com>
2022-03-29 23:15:34 -0600

    refactor: todo file restrictions

--- src/main/kotlin/com/vidbox/file/FileContentController.kt
+++ src/main/kotlin/com/vidbox/file/FileContentController.kt
@@ -14,6 +14,7 @@ import java.security.Principal
 @RestController
 class FileContentController(@Autowired private val fileService: FileService) {
 
+    // TODO: restrict file types
     @RequestMapping(value = ["/upload"], method = [RequestMethod.PUT])
     fun upload(@RequestParam(name = "file", required = true) file: MultipartFile,
                principal: Principal): ResponseEntity<File> {
