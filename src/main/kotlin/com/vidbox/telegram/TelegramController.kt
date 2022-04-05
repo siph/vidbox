ad995a45ff7302da0f006a4e6ff91946e1412cf7
siph <dawkins.chris.dev@gmail.com>
2022-04-04 21:04:45 -0600

    doc: swagger documentation for telegram controller

--- src/main/kotlin/com/vidbox/telegram/TelegramController.kt
+++ src/main/kotlin/com/vidbox/telegram/TelegramController.kt
@@ -2,7 +2,14 @@ package com.vidbox.telegram
 
 import com.vidbox.album.AlbumService
 import com.vidbox.file.FileService
+import com.vidbox.keys.Keys
 import com.vidbox.telegram.model.Response
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,19 +24,51 @@ class TelegramController(@Autowired private val fileService: FileService,
                          @Autowired private val telegramService: TelegramService,
                          @Autowired private val albumService: AlbumService) {
 
+    @Operation(summary = "Send file to telegram")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Successfully sent",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Response::class)
+            )]
+        )
+    )
     @RequestMapping(value = ["/file"], method = [RequestMethod.POST])
-    fun postFileToTelegram(@RequestParam fileId: Long,
-                           @RequestParam(required = false, defaultValue = "") text: String,
-                           @RequestParam chatId: String,
+    fun postFileToTelegram(@RequestParam
+                           @Parameter(description = "Id for requested file")
+                           fileId: Long,
+                           @Parameter(description = "Text to accompany file")
+                           @RequestParam(required = false, defaultValue = "")
+                           text: String,
+                           @RequestParam
+                           @Parameter(description = "Id for telegram channel")
+                           chatId: String,
                            principal: Principal): ResponseEntity<Response> {
         val file = fileService.getFileById(principal.name, fileId)
         return telegramService.postFileToTelegram(file = file, text = text, chatId = chatId)
     }
 
+    @Operation(summary = "Send album to telegram")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "200",
+            description = "Successfully sent",
+            content = [Content(mediaType = "application/json",
+                schema = Schema(implementation = Response::class)
+            )]
+        )
+    )
     @RequestMapping(value = ["/album"], method = [RequestMethod.POST])
-    fun postAlbumToTelegram(@RequestParam albumId: Long,
-                            @RequestParam(required = false, defaultValue = "") text: String,
-                            @RequestParam chatId: String,
+    fun postAlbumToTelegram(@RequestParam
+                            @Parameter(description = "Id for requested album")
+                            albumId: Long,
+                            @RequestParam(required = false, defaultValue = "")
+                            @Parameter(description = "Text to accompany album")
+                            text: String,
+                            @RequestParam
+                            @Parameter(description = "Id for telegram channel")
+                            chatId: String,
                             principal: Principal): List<ResponseEntity<Response>> {
         val album = albumService.getAlbum(principal.name, albumId)
         return telegramService.postAlbumToTelegram(album, text, chatId)
